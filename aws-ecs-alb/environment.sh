810e78e396f017cfd5a59883a3f5a3821c719a70
chuck hutchinson <chuckhutchinson2@yahoo.com>
2017-04-06 17:22:16 -0400

    updated variables

--- aws-ecs-alb/environment.sh
+++ aws-ecs-alb/environment.sh
@@ -1,2 +1,3 @@
 export TF_VAR_admin_cidr_ingress=73.133.89.73/32
-export TF_VAR_tag=prettyprint
+export TF_VAR_tag=springboot
+export TF_VAR_dockerImageUrl=395319207868.dkr.ecr.us-east-1.amazonaws.com/docker/docker-examples:latest
